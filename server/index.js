278ff808ba27d37cde4637fd043855b1aeb9b23d
Adam Tiller <a.j.tiller@gmail.com>
2020-05-12 11:02:28 -0700

    Modify routing to handle POST requests.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
+const jsonParser = bodyParser.json();
 const PORT = 3121;
-const REVIEWS = 'http://localhost:3002';
+const REVIEWS = 'http://localhost:3002/';
 
 app.listen(PORT, () => { console.log(`Now listening on port ${PORT}`); });
 
 app.use('/games/:gameId', express.static('public'));
 
-app.get('/reviews/:gameId', (req, res) => {
-  axios.get(REVIEWS + `/reviews/${req.params.gameId}`)
+app.all('/reviews/:gameId', jsonParser, (req, res) => {
+  const METHOD = req.method;
+  const BODY = req.body;
+  axios({
+    method: METHOD,
+    url: `${REVIEWS}reviews/${req.params.gameId}`,
+    data: JSON.stringify(BODY),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
     .then((response) => {
       res.status(200).send(response.data);
     })
