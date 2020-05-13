a31256d08f4406cd3cb07793ca6cadaa6e1b6bd1
Adam Tiller <a.j.tiller@gmail.com>
2020-05-13 07:25:20 -0700

    Add template for proxy. Add addToCart, Carousel, and Recommended.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 const jsonParser = bodyParser.json();
 const PORT = 3121;
 const REVIEWS = 'http://localhost:3002/';
+const CART = 'http://localhost:3001/';
+const CAROUSEL = 'http://localhost:3003/';
+const RECOMMENDED = 'http://localhost:3005/';
 
 app.listen(PORT, () => { console.log(`Now listening on port ${PORT}`); });
 
@@ -25,5 +28,68 @@ app.all('/reviews/:gameId', jsonParser, (req, res) => {
     .then((response) => {
       res.status(200).send(response.data);
     })
-    .catch((err) => { console.log(err); });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Something went wrong!');
+    });
+});
+
+app.all('/cartapi/:gameId', jsonParser, (req, res) => {
+  const METHOD = req.method;
+  const BODY = req.body;
+  axios({
+    method: METHOD,
+    url: `${CART}cartapi/${req.params.gameId}`,
+    data: JSON.stringify(BODY),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Something went wrong!');
+    });
+});
+
+app.all('/carousel/:gameId', jsonParser, (req, res) => {
+  const METHOD = req.method;
+  const BODY = req.body;
+  axios({
+    method: METHOD,
+    url: `${CAROUSEL}carousel/${req.params.gameId}`,
+    data: JSON.stringify(BODY),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Something went wrong!');
+    });
+});
+
+app.all('/recommended/', jsonParser, (req, res) => {
+  const METHOD = req.method;
+  const BODY = req.body;
+  axios({
+    method: METHOD,
+    url: `${RECOMMENDED}recommended/`,
+    data: JSON.stringify(BODY),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Something went wrong!');
+    });
 });
